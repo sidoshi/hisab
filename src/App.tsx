21762d8931b0ba26ad1d2c9cb9b786e6801a42d1
Siddharth Doshi <sidoshi510@gmail.com>
2025-11-10 12:37:05 +0530

    easy configurable db location

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,12 @@
+import { useEffect } from "react";
 import { Text, View } from "reshaped";
+import { loadDB } from "./db";
 
 function App() {
+  useEffect(() => {
+    loadDB();
+  }, []);
+
   return (
     <View gap={3} align="center" justify="center" height="100vh">
       <Text variant="title-1">Chandan Hisab</Text>
--- src/db/index.ts
+++ src/db/index.ts
@@ -1,5 +1,6 @@
 import { invoke } from "@tauri-apps/api/core";
 import { drizzle } from "drizzle-orm/sqlite-proxy";
+import Database from "@tauri-apps/plugin-sql";
 import * as schema from "./schema";
 
 type Row = {
@@ -7,35 +8,44 @@ type Row = {
   values: string[];
 };
 
-const db = drizzle(
-  async (sql, params, method) => {
-    try {
-      const rows = await invoke<Row[]>("run_sql", {
-        query: { sql, params },
-      });
-      if (rows.length === 0 && method === "get") {
-        /**
-         * 🛠 Workaround for Drizzle ORM SQLite Proxy `.get()` bug
-         *
-         * `.get()` with no results throws due to Drizzle trying to destructure `undefined`.
-         * See: https://github.com/drizzle-team/drizzle-orm/issues/4113
-         *
-         * Until fixed upstream, we return `{}` when rows are empty to avoid crashes.
-         */
-        return {} as { rows: string[] };
-      }
-      return method === "get"
-        ? { rows: rows[0].values }
-        : { rows: rows.map((r) => r.values) };
-    } catch (e: unknown) {
-      console.error("Error from sqlite proxy server: ", e);
-      return { rows: [] };
-    }
-  },
-  {
-    schema,
-    logger: true,
-  }
-);
+let db: ReturnType<typeof drizzle> | null = null;
 
-export default db;
+export async function loadDB() {
+  if (db == null) {
+    // Running Migrations
+    await Database.load("sqlite:database.db");
+
+    db = drizzle(
+      async (sql, params, method) => {
+        try {
+          const rows = await invoke<Row[]>("run_sql", {
+            query: { sql, params },
+          });
+          if (rows.length === 0 && method === "get") {
+            /**
+             * 🛠 Workaround for Drizzle ORM SQLite Proxy `.get()` bug
+             *
+             * `.get()` with no results throws due to Drizzle trying to destructure `undefined`.
+             * See: https://github.com/drizzle-team/drizzle-orm/issues/4113
+             *
+             * Until fixed upstream, we return `{}` when rows are empty to avoid crashes.
+             */
+            return {} as { rows: string[] };
+          }
+          return method === "get"
+            ? { rows: rows[0].values }
+            : { rows: rows.map((r) => r.values) };
+        } catch (e: unknown) {
+          console.error("Error from sqlite proxy server: ", e);
+          return { rows: [] };
+        }
+      },
+      {
+        schema,
+        logger: true,
+      }
+    );
+  }
+
+  return db;
+}
